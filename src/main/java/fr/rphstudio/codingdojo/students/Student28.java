3529900f92c739df8c64b070c5016afeab5559c8
marine <marine.b.58@hotmail.fr>
2019-09-17 16:51:20 +0200

    rotation du vaisseau

--- src/main/java/fr/rphstudio/codingdojo/students/Student28.java
+++ src/main/java/fr/rphstudio/codingdojo/students/Student28.java
@@ -19,8 +19,45 @@ public class Student28 extends PodPlugIn {
     
     //-------------------------------------------------------
     // DECLARE YOUR OWN VARIABLES AND FUNCTIONS HERE
-    
-    
+    public  void speed()
+    {
+        if (getNextCheckPointDistance() > 3)
+            incSpeed(1f);
+        else if (getNextCheckPointDistance() > 1)
+            incSpeed(0.6f);
+    }
+
+    public static float radiantToDegres(float radiant)
+    {
+        int degres = ((int)(radiant / 3.14f)) * 180;
+        return (degres);
+    }
+
+    public static float degresToRadiant(int degres)
+    {
+        float radiant = (degres / 180) * 3.14f;
+        return (radiant);
+    }
+
+    public void tourner()
+    {
+        int x_ship = (int)getShipPositionX();
+        int y_ship = (int)getShipPositionY();
+        int x_check = (int)getCheckPointPositionX(x_ship);
+        int y_check = (int)getCheckPointPositionY(y_ship);
+
+        int dy = y_check - y_ship;
+        int dx = x_check - x_ship;
+        float delta_check = atan2(dy, dx);
+        int res_degres = ((int)delta_check + 360) % 360;
+        int delta_angle = res_degres - (int)getShipAngle();
+        delta_angle = (delta_angle % 360) + 360;
+        if (delta_angle < 180)
+            turn(delta_angle);
+        else
+            turn(delta_angle);
+    }
+
     // END OF VARIABLES/FUNCTIONS AREA
     //-------------------------------------------------------
     
@@ -37,17 +74,13 @@ public class Student28 extends PodPlugIn {
 
         turnTowardNextCheckPoint();
         //moveToNextCheckPoint(0.5f);
-        if (getNextCheckPointDistance() > 3)
-            incSpeed(1f);
-        else if (getNextCheckPointDistance() > 1)
-            incSpeed(0.6f);
+       speed();
         if (getShipBoostLevel() == 100)
-            if (getNextCheckPointDistance() > 3f)
+            if (getNextCheckPointDistance() > 10f)
                 useBoost();
         if (getNextCheckPointDistance() < 0.6)
             incSpeed( -1);
-        if (getNextCheckPointDistance() == 0)
-            moveToNextCheckPoint(1);
+        //if (getNextCheckPointDistance())
         System.out.println(getNextCheckPointDistance());
 
         // END OF CODE AREA
